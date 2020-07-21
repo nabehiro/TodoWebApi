eb05cc8efb2a06f466ba3d10f6bb9d1d78d99bc5
nabehiro <hiroyuki@commerble.com>
2020-07-21 10:35:54 +0900

    cleanup code and add cors

--- TodoWebApi/Models/TodoItem.cs
+++ TodoWebApi/Models/TodoItem.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TodoWebApi.Models
+﻿namespace TodoWebApi.Models
 {
     public class TodoItem
     {
--- TodoWebApi/Startup.cs
+++ TodoWebApi/Startup.cs
@@ -23,12 +23,34 @@ namespace TodoWebApi
             services.AddDbContext<TodoContext>(opt =>
                opt.UseInMemoryDatabase("TodoList"));
 
+            services.AddCors();
+
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(builder =>
+            {
+                builder.WithOrigins("http://localhost:8080")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
